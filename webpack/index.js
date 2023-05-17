ea76878b66a3e2b642df4839beaf00ba09a353f2
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-05-17 16:49:09 +0200

    Added Webpack Encore template

---
+++ webpack/index.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+const getPublicDir = () => {
+    const composerConfig = require(`${ process.cwd() }/composer.json`);
+
+    return composerConfig['extra']['public-dir'] || (fs.existsSync(`${ process.cwd() }/web`) ? 'web' : 'public');
+}
+
+const buildEncore = (layoutDir = 'layout', detectEntries = true) => {
+    const Encore = require('@symfony/webpack-encore');
+
+    Encore
+        .setOutputPath(`${ getPublicDir() }/${ layoutDir }/`)
+        .setPublicPath(`/${ layoutDir }`)
+        .setManifestKeyPrefix('')
+        .cleanupOutputBeforeBuild()
+        .disableSingleRuntimeChunk()
+
+        .enableSassLoader()
+        .enablePostCssLoader()
+        .enableSourceMaps()
+        .enableVersioning(Encore.isProduction())
+
+        .addLoader({
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: ['image-webpack-loader']
+        })
+
+        .configureDevServerOptions(() => ({
+            allowedHosts: 'all',
+            watchFiles: ['config/*', 'contao/**/*', 'src/**/*', 'templates/**/*', 'translations/**/*'],
+        }))
+    ;
+
+    // Automatically detect JavaScript files in the layout folder and add Encore entries
+    if (detectEntries && fs.existsSync(`${ process.cwd() }/${ layoutDir }`)) {
+        fs.readdirSync(`${ process.cwd() }/${ layoutDir }/`).filter(f => f.endsWith('.js')).forEach((file) => {
+            Encore.addEntry(
+                file.substring(0, file.length - 3),
+                `./${ layoutDir }/${ file }`
+            );
+        });
+    }
+
+    // Automatically configure PostCSS if there is no config in the project
+    if (!fs.existsSync(`${ process.cwd() }/postcss.config.js`)) {
+        Encore.enablePostCssLoader((options) => {
+            options.postcssOptions = {
+                plugins: {
+                    'autoprefixer': {},
+                }
+            }
+        });
+    }
+
+    return Encore;
+}
+
+module.exports = {
+    Encore: buildEncore,
+}
