f0854c20c58b7c9ee085dd26f3e2c5a150d69596
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-05-22 10:43:42 +0200

    Automatically use Rector upgrades for PHP 8.1

--- tools/rector/config.php
+++ tools/rector/config.php
@@ -3,12 +3,25 @@
 declare(strict_types=1);
 
 use Contao\Rector\Set\ContaoLevelSetList;
+use Contao\Rector\Set\ContaoSetList;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([getcwd().'/src']);
 
+    $rectorConfig->paths([getcwd().'/src']);
     $rectorConfig->sets([ContaoLevelSetList::UP_TO_CONTAO_413]);
+
+    if (file_exists(getcwd().'/composer.json')) {
+        $composerJson = json_decode(file_get_contents(getcwd().'/composer.json'), true, 512, JSON_THROW_ON_ERROR);
+
+        switch($composerJson['require']['php'] ?? null) {
+            case '^8.1':
+            case '8.1.*':
+                $rectorConfig->sets([LevelSetList::UP_TO_PHP_81, ContaoSetList::ANNOTATIONS_TO_ATTRIBUTES]);
+                break;
+        }
+    }
 
     require_once getcwd().'/vendor/contao/core-bundle/src/Resources/contao/config/constants.php';
 
