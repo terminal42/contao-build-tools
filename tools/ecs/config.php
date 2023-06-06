fe72abb27592247bd281bebbdc6843bb154b1770
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-06 09:13:05 +0200

    Do not change return values on PHP7

--- tools/ecs/config.php
+++ tools/ecs/config.php
@@ -2,12 +2,27 @@
 
 declare(strict_types=1);
 
+use Contao\EasyCodingStandard\Fixer\TypeHintOrderFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([getcwd().'/src']);
     $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
+
+    if (file_exists(getcwd().'/composer.json')) {
+        $composerJson = json_decode(file_get_contents(getcwd().'/composer.json'), true, 512, JSON_THROW_ON_ERROR);
+
+        switch($composerJson['require']['php'] ?? null) {
+            case '^8.1':
+            case '8.1.*':
+                break;
+
+            default:
+                $ecsConfig->skip([TypeHintOrderFixer::class]);
+                break;
+        }
+    }
 
     $ecsConfig->parallel();
     $ecsConfig->lineEnding("\n");
