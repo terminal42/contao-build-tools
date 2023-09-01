b62a26f7c4ba0883ec6921e926c4eab4a92b0dda
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-09-01 16:40:20 +0200

    Fixed missing test() method on deployment

--- src/Deployer.php
+++ src/Deployer.php
@@ -17,6 +17,7 @@ use function Deployer\run;
 use function Deployer\runLocally;
 use function Deployer\set;
 use function Deployer\task;
+use function Deployer\test;
 use function Deployer\upload;
 use function Deployer\warning;
 
