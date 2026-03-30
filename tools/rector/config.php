4eee68aee5e9b6be76a083377a3aaf491e21fedd
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-03-30 15:46:58 +0200

    Remove special handling of PushRequestToRequestStackConstructorRector
    This should never have been necessary but was caused by https://github.com/contao/contao-rector/commit/a70f22715b5084df0f9934919aaf620326d85e30#diff-0987b4885927b582654a4cf10e937080807507340adae7266ab75e8710957996R11-R12

--- tools/rector/config.php
+++ tools/rector/config.php
@@ -27,12 +27,11 @@ return static function (RectorConfig $rectorConfig): void {
 
     if ($contaoConstraint = $composerJson['require']['contao/core-bundle'] ?? $composerJson['require']['contao/manager-bundle'] ?? $composerJson['require-dev']['contao/core-bundle'] ?? $composerJson['require-dev']['contao/manager-bundle'] ?? null) {
         $parsedConstraints = $versionParser->parseConstraints($contaoConstraint);
-        $isContao4 = $parsedConstraints->matches($versionParser->parseConstraints('< 5.0'));
 
         $setList = match (true) {
             $parsedConstraints->matches($versionParser->parseConstraints('< 4.9')) => [],
             $parsedConstraints->matches($versionParser->parseConstraints('< 4.10')) => [ContaoLevelSetList::UP_TO_CONTAO_49],
-            $isContao4 => [ContaoLevelSetList::UP_TO_CONTAO_413],
+            $parsedConstraints->matches($versionParser->parseConstraints('< 5.0')) => [ContaoLevelSetList::UP_TO_CONTAO_413],
             $parsedConstraints->matches($versionParser->parseConstraints('< 5.1')) => [ContaoLevelSetList::UP_TO_CONTAO_50],
             $parsedConstraints->matches($versionParser->parseConstraints('< 5.3')) => [ContaoLevelSetList::UP_TO_CONTAO_51],
             $parsedConstraints->matches($versionParser->parseConstraints('< 5.4')) => [ContaoLevelSetList::UP_TO_CONTAO_53],
@@ -42,11 +41,6 @@ return static function (RectorConfig $rectorConfig): void {
 
         if (!empty($setList)) {
             $rectorConfig->sets($setList);
-        }
-
-        if ($isContao4) {
-            // Request stack constructor argument is not available in Contao 4.13
-            $rectorConfig->skip([PushRequestToRequestStackConstructorRector::class]);
         }
     }
 
